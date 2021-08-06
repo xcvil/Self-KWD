debfaa884e4f4bea15109e69e468019dc6290d1a
xcvil <xzheng.ethz@gmail.com>
2021-08-06 17:44:28 +0200

    debug

--- pretrain_bimoco.sh
+++ pretrain_bimoco.sh
@@ -9,6 +9,7 @@ python main_moco.py \
   --bimoco --cos --mlp \
   --moco-t 0.2 \
   --amp-opt-level O1 \
+  --knn-data "/home/xiaochen/KWD-LT-0.1" \
   --save-dir "output/kuzikus/mocov2/bimoco-geo-color-epochs200/" \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   '/home/xiaochen/pretrain_dataset_256_seed11369'
