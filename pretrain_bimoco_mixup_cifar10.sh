c91df549effd57533b4dc95143ffb70b6239364d
xcvil <xzheng.ethz@gmail.com>
2021-08-08 16:42:40 +0200

    add cifar

---
+++ pretrain_bimoco_mixup_cifar10.sh
@@ -0,0 +1,15 @@
+lr=0.06
+CUDA_VISIBLE_DEVICES=0 \
+python main_cifar.py \
+  -a resnet18 \
+  --lr ${lr} \
+  --workers 2 \
+  --batch-size 64 \
+  --moco-k 4096 \
+  --bimoco --mixup --cos --mlp --rui --replace \
+  --bimoco-gamma 0.5 \
+  --mixup-p 0.3 \
+  --moco-t 0.2 \
+  --amp-opt-level O1 \
+  --save-dir "output/kuzikus/mocov2/cifar-bimoco-mixup0.3-rui-replace-epochs200/" \
+  --dist-url 'tcp://localhost:10003' --multiprocessing-distributed --world-size 1 --rank 0
