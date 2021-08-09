6aca528086211cabc95ae89a3865855193636c34
xcvil <xzheng.ethz@gmail.com>
2021-08-09 23:17:47 +0200

    knn-k grid search

--- knn_search.py
+++ knn_search.py
@@ -19,13 +19,22 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 import torch.nn.functional as F
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+import moco.loader as loader
+import moco.builder as builder
 
 import json
+try:
+    # noinspection PyUnresolvedReferences
+    from apex import amp
+except ImportError:
+    amp = None
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
-
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
@@ -35,30 +44,87 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
                         ' (default: resnet50)')
 parser.add_argument('-j', '--workers', default=32, type=int, metavar='N',
                     help='number of data loading workers (default: 32)')
-parser.add_argument('-p', '--print-freq', default=10, type=int,
-                    metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('--seed', default=None, type=int,
-                    help='seed for initializing training. ')
-parser.add_argument('--gpu', default=0, type=int,
-                    help='GPU id to use.')
-parser.add_argument('--save-dir', default='', type=str, metavar='PATH',
-                    help='path to save checkpoint (default: none)')
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
                          'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.03, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum of SGD solver')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--world-size', default=-1, type=int,
+                    help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int,
+                    help='node rank for distributed training')
+parser.add_argument('--dist-url', default=None, type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default=1, type=int,
+                    help='GPU id to use.')
+parser.add_argument('--multiprocessing-distributed', action='store_true',
+                    help='Use multi-processing distributed training to launch '
+                         'N processes per node, which has N GPUs. This is the '
+                         'fastest way to use PyTorch for either single node or '
+                         'multi node data parallel training')
+parser.add_argument('--save-dir', default='', type=str, metavar='PATH',
+                    help='path to save checkpoint (default: none)')
 
+# moco specific configs:
+parser.add_argument('--bimoco', action='store_true',
+                    help='use two branches MoCo')
+parser.add_argument('--bimoco-gamma', default=0.5, type=float,
+                    help='fraction of MoCo v2 loss')
+parser.add_argument('--moco-dim', default=128, type=int,
+                    help='feature dimension (default: 128)')
+parser.add_argument('--moco-k', default=65536, type=int,
+                    help='queue size; number of negative keys (default: 65536)')
+parser.add_argument('--moco-m', default=0.999, type=float,
+                    help='moco momentum of updating key encoder (default: 0.999)')
+parser.add_argument('--moco-t', default=0.07, type=float,
+                    help='softmax temperature (default: 0.07)')
+
+# options for moco v2
+parser.add_argument('--mlp', action='store_true',
+                    help='use mlp head')
+parser.add_argument('--mixup', action='store_true',
+                    help='use mixup data augmentation')
+parser.add_argument('--aug-color-only', action='store_true',
+                    help='use only color data augmentation')
+parser.add_argument('--aug-geo', action='store_true',
+                    help='use only geometric data augmentation')
+parser.add_argument('--geo-plus', action='store_true',
+                    help='use only geometric data augmentation')
+parser.add_argument('--cos', action='store_true',
+                    help='use cosine lr schedule')
+
+# MixUp augmentation configs:
+parser.add_argument('--mixup-p', default=None, type=float,
+                    help='the prob to apply a mixup aug in a certain iteration')
+parser.add_argument('--replace', action='store_true',
+                    help='whether replace the original loss with mixup loss or not')
+parser.add_argument('--rui', action='store_true',
+                    help='use Rui method')
 # options for mix precision training
 parser.add_argument('--amp-opt-level', type=str, default='O0', choices=['O0', 'O1', 'O2'],
                     help='mixed precision opt level, if O0, no amp is used')
 
-parser.add_argument('--pretrained', default=None, type=str,
-                    help='path to moco pretrained checkpoint')
 # knn monitor
-parser.add_argument('--knn-k', default=20, type=int, help='k in kNN monitor')
+parser.add_argument('--knn-k', default=200, type=int, help='k in kNN monitor')
 parser.add_argument('--knn-t', default=0.1, type=float, help='softmax temperature in kNN monitor; could be different with moco-t')
 parser.add_argument('--knn-data', default='', type=str, metavar='PATH',
                     help='path to dataset of KNN')
@@ -69,7 +135,7 @@ def main():
 
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
-    path = os.path.join(args.save_dir, "config_encoding.json")
+    path = os.path.join(args.save_dir, "config.json")
     with open(path, 'w') as f:
         json.dump(vars(args), f, indent=2)
     print("Full config saved to {}".format(path))
@@ -88,53 +154,106 @@ def main():
         warnings.warn('You have chosen a specific GPU. This will completely '
                       'disable data parallelism.')
 
+    if args.dist_url == "env://" and args.world_size == -1:
+        args.world_size = int(os.environ["WORLD_SIZE"])
+
+    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
+    print('Using distributed training')
+
     ngpus_per_node = torch.cuda.device_count()
     print('there is/are {} GPUs per nodes'.format(ngpus_per_node))
-    main_worker(args.gpu, args)
+    if args.multiprocessing_distributed:
+        # Since we have ngpus_per_node processes per node, the total world_size
+        # needs to be adjusted accordingly
+        args.world_size = ngpus_per_node * args.world_size
+        # Use torch.multiprocessing.spawn to launch distributed processes: the
+        # main_worker process function
+        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    else:
+        # Simply call main_worker function
+        main_worker(args.gpu, ngpus_per_node, args)
 
 
-def main_worker(gpu, args):
+def main_worker(gpu, ngpus_per_node, args):
     args.gpu = gpu
 
+    # suppress printing if not master
+    if args.multiprocessing_distributed and args.gpu != 0:
+        def print_pass(*args):
+            pass
+        builtins.print = print_pass
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    if args.distributed:
+        if args.dist_url == "env://" and args.rank == -1:
+            args.rank = int(os.environ["RANK"])
+        if args.multiprocessing_distributed:
+            # For multiprocessing distributed training, rank needs to be the
+            # global rank among all the processes
+            args.rank = args.rank * ngpus_per_node + gpu
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size, rank=args.rank)
     # create model
     print("=> creating model '{}'".format(args.arch))
-    model = models.__dict__[args.arch](num_classes=2)
-    print(model)
+    if args.bimoco:
+        model = builder.BiMoCo(models.__dict__[args.arch],
+                               args.moco_dim, args.moco_k, args.moco_m, args.moco_t, args.mlp)
+    else:
+        model = builder.MoCo(models.__dict__[args.arch],
+                             args.moco_dim, args.moco_k, args.moco_m, args.moco_t, args.mlp)
 
     if args.gpu is not None:
         torch.cuda.set_device(args.gpu)
         model.cuda(args.gpu)
 
-    # optionally load from a pretrained
-    if args.pretrained:
-        if os.path.isfile(args.pretrained):
-            print("=> loading checkpoint '{}'".format(args.pretrained))
+    if args.distributed:
+        # For multiprocessing distributed, DistributedDataParallel constructor
+        # should always set the single device scope, otherwise,
+        # DistributedDataParallel will use all available devices.
+        if args.gpu is not None:
+            torch.cuda.set_device(args.gpu)
+            model.cuda(args.gpu)
+            # When using a single GPU per process and per
+            # DistributedDataParallel, we need to divide the batch size
+            # ourselves based on the total number of GPUs we have
+            args.batch_size = int(args.batch_size / ngpus_per_node)
+            args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        else:
+            model.cuda()
+            # DistributedDataParallel will divide and allocate batch_size to all
+            # available GPUs if device_ids are not set
+            model = torch.nn.parallel.DistributedDataParallel(model)
+    elif args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+        # comment out the following line for debugging
+        raise NotImplementedError("Only DistributedDataParallel is supported.")
+    else:
+        # AllGather implementation (batch shuffle, queue update, etc.) in
+        # this code only supports DistributedDataParallel.
+        raise NotImplementedError("Only DistributedDataParallel is supported.")
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
             if args.gpu is None:
-                checkpoint = torch.load(args.pretrained)
+                checkpoint = torch.load(args.resume)
             else:
                 # Map model to be loaded to specified single gpu.
                 loc = 'cuda:{}'.format(args.gpu)
-                checkpoint = torch.load(args.pretrained, map_location=loc)
-
-            # rename moco pre-trained keys
-            state_dict = checkpoint['state_dict']
-            epoch = checkpoint['epoch']
-            for k in list(state_dict.keys()):
-                # retain only encoder_q up to before the embedding layer
-                if k.startswith('module.encoder_q') and not k.startswith('module.encoder_q.fc'):
-                    # remove prefix
-                    state_dict[k[len("module.encoder_q."):]] = state_dict[k]
-                # delete renamed or unused k
-                del state_dict[k]
-
-            msg = model.load_state_dict(state_dict, strict=False)
-            assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
-
-            print("=> loaded pre-trained model '{}' in epoch {}".format(args.pretrained, epoch))
+                checkpoint = torch.load(args.resume, map_location=loc)
+            model.load_state_dict(checkpoint['state_dict'])
+            # optimizer = checkpoint['optimizer']
+            if args.amp_opt_level != "O0" and checkpoint['args'].amp_opt_level != "O0":
+                amp.load_state_dict(checkpoint['amp'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
         else:
-            print("=> no checkpoint found at '{}'".format(args.pretrained))
-
-    model = nn.Sequential(*list(model.children())[:-1])
+            print("=> no checkpoint found at '{}'".format(args.resume))
 
     cudnn.benchmark = True
 
--- knn_search.sh
+++ knn_search.sh
@@ -2,6 +2,8 @@ CUDA_VISIBLE_DEVICES=0 \
 python knn_search.py \
   -a resnet50 \
   --workers 2 \
-  --pretrained '/home/xiaochen/journal_output/bimoco-mixup0.3-rui-replace-epochs200-LT/checkpoint_0199.pth.tar' \
+  --resume '/home/xiaochen/journal_output/bimoco-mixup0.3-rui-replace-epochs200-LT/checkpoint_0199.pth.tar' \
   --save-dir "output/features/" \
-  --knn-data "/home/xiaochen/KWD-LT-0.1"
+  --bimoco \
+  --knn-data "/home/xiaochen/KWD-LT-0.1" \
+  --dist-url 'tcp://localhost:10003' --multiprocessing-distributed --world-size 1 --rank 0
