d599679a0420d88f2f34f7f6a8e620ccd1dbb0f4
xcvil <xzheng.ethz@gmail.com>
2021-08-17 11:35:54 +0200

    cifar

--- pretrain_bimoco_mixup_cifar10.sh
+++ pretrain_bimoco_mixup_cifar10.sh
@@ -4,14 +4,17 @@ python main_cifar.py \
   -a resnet18 \
   --lr ${lr} \
   --workers 2 \
-  --batch-size 64 \
+  --batch-size 512 \
+  --epochs 800 \
   --moco-k 4096 \
   --bimoco --mixup --cos --mlp --rui --replace \
-  --bimoco-gamma 0.5 \
+  --bimoco-gamma 0.9 \
   --mixup-p 0.3 \
   --moco-t 0.1 \
   --moco-m 0.99 \
-  --wd 0.0005\
+  --wd 0.0005 \
+  --knn-k 200 \
+  --knn-t 0.1 \
   --amp-opt-level O1 \
-  --save-dir "output/kuzikus/mocov2/cifar-bimoco-mixup0.3-rui-replace-epochs200/" \
-  --dist-url 'tcp://localhost:10003' --multiprocessing-distributed --world-size 1 --rank 0
+  --save-dir "output/kuzikus/mocov2/cifar-bimoco-gamma0.9-mixup0.3-rui-replace-epochs200/" \
+  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0
